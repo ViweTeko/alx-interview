baf329767b8119d7ff6b65f2aba1b63867869f1e
ViweTeko <iamviwe.teko@gmail.com>
2024-07-25 05:22:57 +0200

    Logic issues

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,11 +8,11 @@ def validUTF8(data):
     m1 = 1 << 7
     m2 = 1 << 6
     for byte in data:
-        byte_mask 1 << 7
+        byte_mask = 1 << 7
         if bytes_num == 0:
             while byte & byte_mask:
                 bytes_num += 1
-                byte_mask = byte_mask >> 1
+                byte_mask >>= 1
             if bytes_num == 0:
                 continue
             if bytes_num == 1 or bytes_num > 4:
@@ -21,6 +21,4 @@ def validUTF8(data):
             if not (byte & m1 and not (byte & m2)):
                 return False
         bytes_num -= 1
-    if bytes_num == 0:
-        return True
-    return False
+    return bytes_num == 0
